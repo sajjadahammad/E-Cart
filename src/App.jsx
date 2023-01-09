e869e69148e084bed4497472376228123b05570e
sajjadahammed <sajjad08@protonmail.com>
2023-01-09 10:52:31 +0530

    Intial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Shop from "./pages/shop/Shop"
+import Cart from "./pages/cart/Cart";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/"  element={<Shop />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/shop/Shop.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { PRODUCTS } from '../../products'
+
+function Shop  () {
+  return (
+    <div className='shop'>
+      <div className='shopTitle'>
+        <h1>e-cart</h1>
+        </div>
+        <div className='products'>
+
+        </div>
+       </div>
+  )
+}
+
+export default Shop
